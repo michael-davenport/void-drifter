7e9ac68e5d786752eb73375ea0d986292d71258e
michael-davenport <michael.davenport.az@gmail.com>
2020-03-07 23:48:44 -0700

    minor changes

--- scenes/World.gd
+++ scenes/World.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+func _ready() -> void:
+	$TextureRect.modulate = Color(randf(),randf(),randf(),0.15)
+	$TextureRect2.modulate = Color(randf(),randf(),randf(),0.15)
+
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
